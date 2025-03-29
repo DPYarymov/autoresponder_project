73a2fd0c6dc4a80b4bdb02daa93e8833690aa68a
DPYarymov <D.Yarymov@ya.ru>
2025-03-29 21:28:40 +0300

    Дописана прога. Много изменений

--- find_pattern.py
+++ find_pattern.py
@@ -1,82 +1,117 @@
-import os
+import sys
 import re
 import time
 
-PROJECT_FOLDERPATH = r"C:\programming\Vovan"
+FROMTECH_FILEPATH = r"C:\programming\Vovan\FromTech_voicemail.txt"
 AUTORESPONDER_FILEPATH = r"C:\programming\Vovan\autoresponder.txt"
 RESULT_FILE_FILEPATH = r"C:\programming\Vovan\result_file.txt"
 
 
-def file_modification(file):   # удаление из файла ::"True", возврат modify_file
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        elapsed_time = (end_time - start_time) * 1000
+        print(f"Время выполнения функции '{func.__name__}': {elapsed_time:.4f} мс")
+        return result
+    return wrapper
 
-    modify_file = []
+
+def read_files(FILEPATH):
 
     try:
-        with open(os.path.join(PROJECT_FOLDERPATH, file), 'r', encoding='utf-8') as orig_file:
-            for line in orig_file:
-                line = line.rstrip()
-                if line.endswith('::"true"'):
-                    line = line[:-8]
-                modify_file.append(line)
+        with open(FILEPATH, 'r', encoding='utf-8') as file:
 
+            file_list = []
+            for string in file:
+                string = string.strip()
+                file_list.append(string)
+
+            return file_list
     except Exception as e:
         print(f"Произошла ошибка при чтении файла: {e}")
+        sys.exit()
+
+
+def delete_end_strings_in_lisr(list):   # удаление из файла ::"True", возврат modify_file
+
+    without_str_ends_fromtech_list = []
+    for str in list:
+        if str.endswith('::"true"'):
+            str = str[:-8]
+            without_str_ends_fromtech_list.append(str)
+    return without_str_ends_fromtech_list
+
+
+def compile_regexes(list):   # компилируем список с рег. выраж.
+
+    if list is None:
         return None
 
-    return modify_file
-
-
-def file_compiletion(modified_file_list):   # компилируем список с рег. выраж.
-
-    if modified_file_list is None:
-        return None
-
-    compiled_regexes = [re.compile(regex) for regex in modified_file_list]
+    compiled_regexes = [re.compile(regex) for regex in list]
     return compiled_regexes
 
+@timer
+def search_for_matches(compiled_regexes_list, string, without_str_ends_fromtech_list):
+    start_time = time.perf_counter()
+    # match_found = False
+    result = None
 
-def search_for_matches(compiled_regexes, modified_file_list):   # поиск совпадения строки с рег.выр.
+    for i, compiled_regular in enumerate(compiled_regexes_list):
+        if compiled_regular.search(string):
+            end_time = time.perf_counter()
+            elapsed_time = (end_time - start_time) * 1000
+            result = (f"{string}|{without_str_ends_fromtech_list[i]}|{elapsed_time:.6f} мс\n")
+            break
 
-    if compiled_regexes is None or modified_file_list is None:
-        print("Ошибка: Нет компилированных регулярных выражений или измененных строк для поиска.")
+    if result is None:
+        end_time = time.perf_counter()
+        elapsed_time = (end_time - start_time) * 1000
+        result = (f"{string}|'Нет совпадений'| {elapsed_time:.6f} мс\n")
+
+    return result
+
+
+def search_function(autoresponder_list, compiled_regexes_list,
+                    without_str_ends_fromtech_list):  # поиск совпадения строки с рег.выр.
+
+    result_list = []
+
+    if compiled_regexes_list is None or without_str_ends_fromtech_list is None:
+        print("Ошибка: Нет компилированных регулярных выражений или измененных строк для поиска")
         return
 
+    for string in autoresponder_list:
+        if not string:
+            continue
+
+        result_string = search_for_matches(compiled_regexes_list, string, without_str_ends_fromtech_list)
+        result_list.append(result_string)
+
+    return result_list
+
+def wright_file(list):
     try:
-        with open(AUTORESPONDER_FILEPATH, 'r', encoding='utf-8') as autoresponder, \
-             open(RESULT_FILE_FILEPATH, 'w', encoding='utf-8') as result_file:
+        with open(RESULT_FILE_FILEPATH, 'w', encoding='utf-8') as result_file:
+            for i in list:
+                result_file.write(i)
 
-            for string in autoresponder:
-                start_time = time.perf_counter()
-                string = string.strip()
-                if not string:
-                    continue
-                match_found = False
-                for i, compiled_regular in enumerate(compiled_regexes):
-                    if compiled_regular.search(string):
-                        end_time = time.perf_counter()
-                        elapsed_time = (end_time - start_time)*1000
-                        result_file.write(f"{string}|{modified_file_list[i]}|{elapsed_time:.6f} мс\n")
-                        match_found = True
-                        break
-                    # else:
-                    #     end_time = time.perf_counter()
-                    #     elapsed_time = (end_time - start_time)*1000
-                if not match_found:
-                    result_file.write(f"{string}|'Нет совпадений'|: {elapsed_time:.6f} мс\n")
-
-    except FileNotFoundError:
-        print("Один из файлов не найден.")
-        return None
     except Exception as e:
         print(f"Произошла ошибка: {e}")
-        return None
+        sys.exit()
 
 
 if __name__ == "__main__":
 
-    modified_file = file_modification("FromTech_voicemail.txt")
+    fromtech_list = read_files(FROMTECH_FILEPATH)
 
-    compiled_regexes = file_compiletion(modified_file)
+    autoresponder_list = read_files(AUTORESPONDER_FILEPATH)
 
-    search_for_matches(compiled_regexes, modified_file)
+    without_str_ends_fromtech_list = delete_end_strings_in_lisr(fromtech_list)
 
+    compiled_regexes_list = compile_regexes(without_str_ends_fromtech_list)
+
+    result_list = search_function(autoresponder_list, compiled_regexes_list, without_str_ends_fromtech_list)
+
+    wright_file(result_list)
